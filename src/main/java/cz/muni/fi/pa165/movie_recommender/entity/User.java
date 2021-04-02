51d901d7a1c33aa156721e02f7342f07747735cb
xjevocin <jevocin@mail.muni.cz>
2021-04-02 11:28:28 +0200

    Implement user entity with jpa persistence

--- src/main/java/cz/muni/fi/pa165/movie_recommender/entity/User.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/entity/User.java
@@ -1,4 +1,59 @@
 package cz.muni.fi.pa165.movie_recommender.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = false)
+    private String mailAddress;
+
+    public User() {
+    }
+
+    public User(String username, String mailAddress) {
+        this.username = username;
+        this.mailAddress = mailAddress;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return username.equals(user.username) && mailAddress.equals(user.mailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, mailAddress);
+    }
 }
