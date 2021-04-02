031bc345fbb5ec0219437bff2eaf21fdcf2b437e
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-04-02 11:43:35 +0200

    Added fixes to Rating entity class

--- src/main/java/cz/muni/fi/pa165/movie_recommender/entity/Rating.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/entity/Rating.java
@@ -1,10 +1,8 @@
 package cz.muni.fi.pa165.movie_recommender.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Rating {
@@ -13,22 +11,38 @@ public class Rating {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @ManyToOne
+    private Movie movie;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+
+    @NotNull
     private int overallScore;
 
+    @NotNull
     private int originality;
 
+    @NotNull
     private int soundtrack;
 
+    @NotNull
     private int narrative;
 
+    @NotNull
     private int cinematography;
 
+    @NotNull
     private int depth;
 
     public Rating() {
     }
 
-    public Rating(int overallScore, int originality, int soundtrack, int narrative, int cinematography, int depth) {
+    public Rating(@NotNull Movie movie, @NotNull User user, @NotNull int overallScore, @NotNull int originality, @NotNull int soundtrack, @NotNull int narrative, @NotNull int cinematography, @NotNull int depth) {
+        this.movie = movie;
+        this.user = user;
         this.overallScore = overallScore;
         this.originality = originality;
         this.soundtrack = soundtrack;
@@ -43,6 +57,22 @@ public class Rating {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public int getOverallScore() {
@@ -100,6 +130,8 @@ public class Rating {
 
         Rating rating = (Rating) o;
 
+        if (!getMovie().equals(rating.getMovie())) return false;
+        if (!getUser().equals(rating.getUser())) return false;
         if (getCinematography() != rating.getCinematography()) return false;
         if (getDepth() != rating.getDepth()) return false;
         if (getNarrative() != rating.getNarrative()) return false;
@@ -110,6 +142,6 @@ public class Rating {
 
     @Override
     public int hashCode() {
-        return Objects.hash(overallScore, originality, soundtrack, narrative, cinematography, depth);
+        return Objects.hash(movie, user, overallScore, originality, soundtrack, narrative, cinematography, depth);
     }
 }
