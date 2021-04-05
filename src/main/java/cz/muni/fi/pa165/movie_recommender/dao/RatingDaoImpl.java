11381289b1865d3ed7a8eb432449a0fc6508bfdb
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-04-05 09:44:23 +0200

    Implemented all methods in RatingDaoImpl

--- src/main/java/cz/muni/fi/pa165/movie_recommender/dao/RatingDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/dao/RatingDaoImpl.java
@@ -3,6 +3,8 @@ package cz.muni.fi.pa165.movie_recommender.dao;
 import cz.muni.fi.pa165.movie_recommender.entity.Rating;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
@@ -13,28 +15,31 @@ import java.util.List;
 @Repository
 public class RatingDaoImpl implements RatingDao {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public void create(Rating rating) {
-
+        entityManager.persist(rating);
     }
 
     @Override
     public List<Rating> findAll() {
-        return null;
+        return entityManager.createQuery("select r from Rating r", Rating.class).getResultList();
     }
 
     @Override
     public Rating findById(Long id) {
-        return null;
+        return entityManager.find(Rating.class, id);
     }
 
     @Override
     public void update(Rating rating) {
-
+        entityManager.merge(rating);
     }
 
     @Override
     public void remove(Rating rating) {
-
+        entityManager.remove(rating);
     }
 }
