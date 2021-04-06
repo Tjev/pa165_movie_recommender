25a9d33a92c019a638b8d30f8cd35bedbb0248a8
Jiří Papoušek <jiri.pap@gmail.com>
2021-04-06 11:42:34 +0200

    Add implementation of PersonDaoImpl

--- src/main/java/cz/muni/fi/pa165/movie_recommender/dao/PersonDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/dao/PersonDaoImpl.java
@@ -2,7 +2,10 @@ package cz.muni.fi.pa165.movie_recommender.dao;
 
 import cz.muni.fi.pa165.movie_recommender.dao.PersonDao;
 import cz.muni.fi.pa165.movie_recommender.entity.Person;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
@@ -10,35 +13,42 @@ import java.util.List;
  *
  * @author Jiri Papousek
  */
+@Repository
 public class PersonDaoImpl implements PersonDao {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Override
     public void create(Person person) {
-
+        em.persist(person);
     }
 
     @Override
     public List<Person> findAll() {
-        return null;
+        return em.createQuery("select p from Person p").getResultList();
     }
 
     @Override
     public Person findById(Long id) {
-        return null;
+        return em.find(Person.class, id);
     }
 
     @Override
     public List<Person> findByName(String name) {
-        return null;
+        return em.createQuery("select p from Person p where p.name = :name")
+                .setParameter("name", name)
+                .getResultList();
     }
 
     @Override
     public void update(Person person) {
-
+        em.merge(person);
     }
 
     @Override
     public void remove(Person person) {
-
+        em.remove(person);
     }
+
 }
