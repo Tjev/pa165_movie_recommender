b66b3bd6624b367dc388e0f43ae1e87584a9c131
Radoslav Chudovsky <469069@mail.muni.cz>
2021-04-06 16:43:44 +0200

    Implement MovieDaoImpl

--- src/main/java/cz/muni/fi/pa165/movie_recommender/dao/MovieDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/dao/MovieDaoImpl.java
@@ -1,7 +1,11 @@
 package cz.muni.fi.pa165.movie_recommender.dao;
 
 import cz.muni.fi.pa165.movie_recommender.entity.Movie;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
@@ -9,37 +13,46 @@ import java.util.List;
  * in-memory data source
  *
  * @author Radoslav Chudovsky
- *
  */
+@Repository
+@Transactional
 public class MovieDaoImpl implements MovieDao {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Override
     public void create(Movie movie) {
-
+        em.persist(movie);
     }
 
     @Override
     public List<Movie> findAll() {
-        return null;
+        return em.createQuery("select m from Movie m", Movie.class).getResultList();
     }
 
     @Override
     public Movie findById(long id) {
-        return null;
+        return em.find(Movie.class, id);
     }
 
     @Override
     public List<Movie> findByTitle(String title) {
-        return null;
+        return em.createQuery("select m from Movie m where m.title=:title", Movie.class)
+                .setParameter("title", title)
+                .getResultList();
     }
 
     @Override
     public void update(Movie movie) {
-
+        em.merge(movie);
     }
 
     @Override
     public void remove(Movie movie) {
-
+        if (!em.contains(movie)) {
+            movie = em.merge(movie);
+        }
+        em.remove(movie);
     }
 }
