3e9269ee2e22113aff54d74f32fa21602e320238
xjevocin <jevocin@mail.muni.cz>
2021-04-06 13:21:44 +0200

    Implement User DAO

--- src/main/java/cz/muni/fi/pa165/movie_recommender/dao/UserDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/dao/UserDaoImpl.java
@@ -1,7 +1,10 @@
 package cz.muni.fi.pa165.movie_recommender.dao;
 
 import cz.muni.fi.pa165.movie_recommender.entity.User;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /**
@@ -9,34 +12,46 @@ import java.util.List;
  *
  * @author Tomas Jevocin
  */
+@Repository
 public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
     public void create(User user) {
-
+        em.persist(user);
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        return em.createQuery("select u from User u", User.class).getResultList();
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        return em.createQuery("select u from User u where u.id = :id", User.class)
+                .setParameter("id", id)
+                .getSingleResult();
     }
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        return em.createQuery("select u from User u where u.username = :username", User.class)
+                .setParameter("username", username)
+                .getResultList();
     }
 
     @Override
     public void update(User user) {
-
+        em.merge(user);
     }
 
     @Override
     public void remove(User user) {
-
+        if (! em.contains(user)) {
+            user = em.merge(user);
+        }
+        em.remove(user);
     }
 }
