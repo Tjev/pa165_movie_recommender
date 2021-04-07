c34f327fceda8dac38e54f20827661f366221d8f
Jiří Papoušek <jiri.pap@gmail.com>
2021-04-07 14:49:48 +0200

    Add Transactional annotation

--- src/main/java/cz/muni/fi/pa165/movie_recommender/dao/PersonDaoImpl.java
+++ src/main/java/cz/muni/fi/pa165/movie_recommender/dao/PersonDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -13,6 +14,7 @@ import java.util.List;
  * @author Jiri Papousek
  */
 @Repository
+@Transactional
 public class PersonDaoImpl implements PersonDao {
 
     @PersistenceContext
