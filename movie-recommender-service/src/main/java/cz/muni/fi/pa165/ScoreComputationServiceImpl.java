e158c0e8eb5d3ad564255eb2ec800af1cbe57f3b
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-04-26 22:49:17 +0200

    Implement all methods in ScoreComputationServiceImpl

--- movie-recommender-service/src/main/java/cz/muni/fi/pa165/ScoreComputationServiceImpl.java
+++ movie-recommender-service/src/main/java/cz/muni/fi/pa165/ScoreComputationServiceImpl.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Stream;
 
 @Service
 public class ScoreComputationServiceImpl implements ScoreComputationService {
@@ -40,34 +37,58 @@ public class ScoreComputationServiceImpl implements ScoreComputationService {
 
     @Override
     public BigDecimal getOverallScoreForMovie(Movie movie) {
-        return null;
+        Set<Rating> ratings = getRatings(movie);
+
+        BigDecimal score = BigDecimal.ZERO;
+
+        for (Rating rating : ratings) {
+            score = score.add(getOverallScoreForRating(rating));
+        }
+
+        return score.divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
     }
 
     @Override
     public BigDecimal getOriginalityScoreForMovie(Movie movie) {
-        Movie persistedMovie = movieDao.findById(movie.getId());
-        Set<Rating> ratings = persistedMovie.getRatings();
-        int score = ratings.stream().map(Rating::getOriginality).mapToInt(i -> i).sum();
+        Set<Rating> ratings = getRatings(movie);
+        int score = ratings.stream().mapToInt(Rating::getOriginality).sum();
+
         return new BigDecimal(score).divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
     }
 
     @Override
     public BigDecimal getSoundtrackScoreForMovie(Movie movie) {
-        return null;
+        Set<Rating> ratings = getRatings(movie);
+        int score = ratings.stream().mapToInt(Rating::getSoundtrack).sum();
+
+        return new BigDecimal(score).divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
     }
 
     @Override
     public BigDecimal getNarrativeScoreForMovie(Movie movie) {
-        return null;
+        Set<Rating> ratings = getRatings(movie);
+        int score = ratings.stream().mapToInt(Rating::getNarrative).sum();
+
+        return new BigDecimal(score).divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
     }
 
     @Override
     public BigDecimal getCinematographyScoreForMovie(Movie movie) {
-        return null;
+        Set<Rating> ratings = getRatings(movie);
+        int score = ratings.stream().mapToInt(Rating::getCinematography).sum();
+
+        return new BigDecimal(score).divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
     }
 
     @Override
     public BigDecimal getDepthScoreForMovie(Movie movie) {
-        return null;
+        Set<Rating> ratings = getRatings(movie);
+        int score = ratings.stream().mapToInt(Rating::getDepth).sum();
+
+        return new BigDecimal(score).divide(new BigDecimal(ratings.size()), RoundingMode.HALF_EVEN);
+    }
+
+    private Set<Rating> getRatings(Movie movie) {
+        return movieDao.findById(movie.getId()).getRatings();
     }
 }
