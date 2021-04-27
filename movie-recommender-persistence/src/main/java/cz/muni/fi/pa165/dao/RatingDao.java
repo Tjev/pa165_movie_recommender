68279397750ee0ccd759ad8edd55e64b29dff56d
Jiří Papoušek <jiri.pap@gmail.com>
2021-04-27 14:25:26 +0200

    Add findByUser and findByMovie methods to RatingDao

--- movie-recommender-persistence/src/main/java/cz/muni/fi/pa165/dao/RatingDao.java
+++ movie-recommender-persistence/src/main/java/cz/muni/fi/pa165/dao/RatingDao.java
@@ -1,8 +1,11 @@
 package cz.muni.fi.pa165.dao;
 
+import cz.muni.fi.pa165.entity.Movie;
 import cz.muni.fi.pa165.entity.Rating;
+import cz.muni.fi.pa165.entity.User;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Dao for manipulation with Rating entities.
@@ -32,6 +35,22 @@ public interface RatingDao {
      * @return rating matching the id
      */
     Rating findById(Long id);
+
+    /**
+     * Retrieves ratings for the movie from the data source
+     *
+     * @param movie for which ratings should be retrieved
+     * @return list of ratings
+     */
+    List<Rating> findByMovie(Movie movie);
+
+    /**
+     * Retrieves ratings for the user from the data source
+     *
+     * @param user for which ratings should be retrieved
+     * @return list of ratings
+     */
+    List<Rating> findByUser(User user);
 
     /**
      * Merges the rating by its ID with its current representation in the data source.
