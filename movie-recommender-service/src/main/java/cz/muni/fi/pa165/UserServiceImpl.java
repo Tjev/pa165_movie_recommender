3ec7fe1dd21873f841b024381a4e32ca900c6c13
Tjev <tomasjevocin@gmail.com>
2021-04-27 20:42:24 +0200

    Add exception handling to user service

--- movie-recommender-service/src/main/java/cz/muni/fi/pa165/UserServiceImpl.java
+++ movie-recommender-service/src/main/java/cz/muni/fi/pa165/UserServiceImpl.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165;
 
 import cz.muni.fi.pa165.dao.UserDao;
 import cz.muni.fi.pa165.entity.User;
+import cz.muni.fi.pa165.exceptions.ServiceLayerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,40 +29,94 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void registerUser(User user, String password) {
+        if (user == null) {
+            throw new IllegalArgumentException("Movie parameter is null.");
+        }
+
+        if (password == null) {
+            throw new IllegalArgumentException("Password parameter is null.");
+        }
+
+        if (password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty.");
+        }
+
         String passwordHash = encoder.encode(password);
         user.setPasswordHash(passwordHash);
-        userDao.create(user);
+
+        try {
+            userDao.create(user);
+        } catch (Exception e) {
+            throw new ServiceLayerException("Error occurred while creating User.", e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return userDao.findAll();
+        try {
+            return userDao.findAll();
+        } catch (Exception e) {
+            throw new ServiceLayerException("Error occurred while retrieving all users.");
+        }
     }
 
     @Override
     public User findUserById(Long id) {
-        return userDao.findById(id);
+        return getPersistedUserById(id);
     }
 
     @Override
     public User findUserByEmailAddress(String emailAddress) {
-        return userDao.findByEmailAddress(emailAddress);
+        if (emailAddress == null) {
+            throw new IllegalArgumentException("Email address parameter is null.");
+        }
+
+        if (emailAddress.isEmpty()) {
+            throw new IllegalArgumentException("Email address cannot be empty.");
+        }
+        try {
+            return userDao.findByEmailAddress(emailAddress);
+        } catch (Exception e) {
+            throw new ServiceLayerException("Error occurred while retrieving user by email address.", e);
+        }
     }
 
     @Override
     public User findUserByUsername(String username) {
-        return userDao.findByUsername(username);
+        if (username == null) {
+            throw new IllegalArgumentException("Username parameter is null.");
+        }
+
+        if (username.isEmpty()) {
+            throw new IllegalArgumentException("Username address cannot be empty.");
+        }
+        try {
+            return userDao.findByUsername(username);
+        } catch (Exception e) {
+            throw new ServiceLayerException("Error occurred while retrieving user by username.", e);
+        }
     }
 
     @Override
     public boolean authenticate(User user, String password) {
-        User persistedUser = userDao.findById(user.getId());
+        User persistedUser = getPersistedUserById(user.getId());
         return encoder.matches(password, persistedUser.getPasswordHash());
     }
 
     @Override
     public boolean isAdmin(User user) {
-        User persistedUser = userDao.findById(user.getId());
+        User persistedUser = getPersistedUserById(user.getId());
         return persistedUser.isAdmin();
+    }
+
+    private User getPersistedUserById(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id parameter is null.");
+        }
+        try {
+            return userDao.findById(id);
+        } catch (Exception e) {
+            throw new ServiceLayerException("Error occurred while retrieving persisted user by id.", e);
+        }
     }
 }
