c00efdc4ddc19df1e0f6538cf424aa7ad362baa5
Tjev <tomasjevocin@gmail.com>
2021-04-30 00:00:50 +0200

    Fix User Entity toString method

--- movie-recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
+++ movie-recommender-persistence/src/main/java/cz/muni/fi/pa165/entity/User.java
@@ -115,6 +115,7 @@ public class User implements Serializable {
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
+                ", admin=" + admin +
                 ", emailAddress='" + emailAddress + '\'' +
                 '}';
     }
