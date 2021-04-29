9114152600e02e8277d5acde83eb7433cc1be845
Tjev <tomasjevocin@gmail.com>
2021-04-29 18:02:07 +0200

    Implement User related DTO classes

---
+++ movie-recommender-api/src/main/java/cz/fi/muni/pa165/dto/UserAuthenticateDTO.java
@@ -0,0 +1,32 @@
+package cz.fi.muni.pa165.dto;
+
+/**
+ * DTO class representing authentication of User entities.
+ *
+ * @author Tomas Jevocin
+ */
+public class UserAuthenticateDTO {
+
+    private Long userId;
+    private String password;
+
+    public Long getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(Long userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
