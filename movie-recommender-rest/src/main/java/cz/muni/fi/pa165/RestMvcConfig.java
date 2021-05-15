1710a74803d40b2a4f1553c31c9e578d043b050f
Jiří Papoušek <jiri.pap@gmail.com>
2021-05-15 15:17:11 +0200

    Change MVC config in order to serialize LocalDate variables

--- movie-recommender-rest/src/main/java/cz/muni/fi/pa165/RestMvcConfig.java
+++ movie-recommender-rest/src/main/java/cz/muni/fi/pa165/RestMvcConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.*;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -35,6 +36,7 @@ public class RestMvcConfig implements WebMvcConfigurer {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH));
 
