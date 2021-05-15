1a25c1838d97b88b03de9081b428b2d162d3a254
chudovskyr <469069@mail.muni.cz>
2021-05-15 12:26:25 +0200

    Add sample-data module

---
+++ movie-recommender-sample-data/src/main/java/cz/muni/fi/pa165/SampleDataLoadingFacade.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165;
+
+import java.io.IOException;
+
+/**
+ * @author Jiri Papousek
+ */
+public interface SampleDataLoadingFacade {
+    void loadData() throws IOException;
+}
