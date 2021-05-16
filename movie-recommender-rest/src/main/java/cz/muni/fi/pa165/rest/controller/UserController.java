8a6d128c387c99e673b44296133ccbb819c9a608
Tjev <tomasjevocin@gmail.com>
2021-05-16 18:44:54 +0200

    Fix UserController class

--- movie-recommender-rest/src/main/java/cz/muni/fi/pa165/rest/controller/UserController.java
+++ movie-recommender-rest/src/main/java/cz/muni/fi/pa165/rest/controller/UserController.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.rest.controller;
 import cz.muni.fi.pa165.dto.user.UserAuthenticateDTO;
 import cz.muni.fi.pa165.dto.user.UserDTO;
 import cz.muni.fi.pa165.dto.user.UserDetailedDTO;
+import cz.muni.fi.pa165.dto.user.UserRegisterDTO;
 import cz.muni.fi.pa165.facade.UserFacade;
 import org.slf4j.Logger;
 import org.springframework.http.MediaType;
@@ -28,18 +29,18 @@ public class UserController {
     /**
      * Register a user into the system.
      *
-     * curl -X POST -i -H "Content-Type: application/json" --data '{"username": "TomWiseau1", "emailAddress": "tom.wiseau@gmail.com"}' http://localhost:8080/pa165/rest/users/register?password=OhHiMark
+     * curl -X PUT -i -H "Content-Type: application/json" --data '{"username": "TomWiseau1", "emailAddress": "tom.wiseau@gmail.com", "password": "OhHiMark"}' http://localhost:8080/pa165/rest/users/register
      *
-     * @param userDTO user to be registered
+     * @param userRegisterDTO user to be registered
      * @return detailed DTO of the user after its creation
      */
     @RequestMapping(value = "/register", method = RequestMethod.PUT,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public final UserDetailedDTO register(@RequestBody UserDTO userDTO, String password) {
-        logger.debug("rest register({})", userDTO);
+    public final UserDetailedDTO register(@RequestBody UserRegisterDTO userRegisterDTO) {
+        logger.debug("rest register({})", userRegisterDTO);
 
-        return userFacade.register(userDTO, password).get();
+        return userFacade.register(userRegisterDTO).get();
     }
 
     /**
@@ -60,13 +61,13 @@ public class UserController {
     /**
      * Find user by its username.
      *
-     * curl -X GET -i http://localhost:8080/pa165/rest/users/find-by-username?name=TomWiseau1
+     * curl -X GET -i http://localhost:8080/pa165/rest/users/find-by-username?username=TomWiseau1
      *
      * @param username username of the user to be found
      * @return detailed DTO of the found user
      */
     @RequestMapping(value = "/find-by-username", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public final UserDetailedDTO findByUsername(@RequestBody String username) {
+    public final UserDetailedDTO findByUsername(@RequestParam String username) {
         logger.debug("rest findByUsername({})", username);
 
         return userFacade.findByUsername(username).get();
@@ -75,13 +76,13 @@ public class UserController {
     /**
      * Find user by its email address.
      *
-     * curl -X GET -i --data '{"emailAddress}' http://localhost:8080/pa165/rest/users/find-by-username?emailAddress=tom%2ewiseau%40gmail%2ecom
+     * curl -X GET -i http://localhost:8080/pa165/rest/users/find-by-mail?emailAddress=tom.wiseau%40gmail.com
      *
      * @param emailAddress email address of the user to be found
      * @return detailed DTO of the found user
      */
     @RequestMapping(value = "/find-by-mail", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public final UserDetailedDTO findByEmailAddress(@RequestBody String emailAddress) {
+    public final UserDetailedDTO findByEmailAddress(@RequestParam String emailAddress) {
         logger.debug("rest findByEmailAddress({})", emailAddress);
 
         return userFacade.findByEmailAddress(emailAddress).get();
@@ -90,7 +91,7 @@ public class UserController {
     /**
      * Authenticate user against his stored credentials.
      *
-     * curl -X POST -i -H "Content-Type: application/json" --data '{"id": "1", "password": "Ididnothither!"}' http://localhost:8080/pa165/rest/users/auth
+     * curl -X POST -i -H "Content-Type: application/json" --data '{"id": "1", "password": "OhHiMark"}' http://localhost:8080/pa165/rest/users/auth
      *
      * @param userAuthenticateDTO user to be authenticated
      * @return true if the authentication was successful, false otherwise
@@ -99,7 +100,7 @@ public class UserController {
                     consumes = MediaType.APPLICATION_JSON_VALUE,
                     produces = MediaType.APPLICATION_JSON_VALUE)
     public final Boolean authenticate(@RequestBody UserAuthenticateDTO userAuthenticateDTO) {
-        logger.debug("rest authenticate user ({})", userAuthenticateDTO.getUserId());
+        logger.debug("rest authenticate user ({})", userAuthenticateDTO.getId());
 
         return userFacade.authenticate(userAuthenticateDTO).get();
     }
@@ -153,7 +154,7 @@ public class UserController {
     /**
      * Update the given user.
      *
-     * curl -X PUT -i -H "Content-Type: application/json" --data '{"id": "1", "username": "TomWiseau1, "emailAddress": "hi.doggy@gmail.com"}' http://localhost:8080/pa165/rest/users/update
+     * curl -X PUT -i -H "Content-Type: application/json" --data '{"id": "1", "username": "TomWiseau1", "emailAddress": "hi.doggy@gmail.com"}' http://localhost:8080/pa165/rest/users/update
      *
      * @param userDetailedDTO DTO of the user to be updated
      * @return detailed DTO of the user after update operation
