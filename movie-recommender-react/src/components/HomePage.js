9d2021d5ff9404b0b350b66a21eed5cf43caaa89
Tjev <tomasjevocin@gmail.com>
2021-05-23 17:41:26 +0200

    Add superb spinning fi muni logo to landing page

--- movie-recommender-react/src/components/HomePage.js
+++ movie-recommender-react/src/components/HomePage.js
@@ -1,7 +1,12 @@
 export function HomePage() {
     return (
         <div>
-            <h1>Movie Recommender System</h1>
+            <div>
+                <h1>Movie Recommender System</h1>
+            </div>
+            <div>
+                <img src='https://www.fi.muni.cz/usr/pelikan/FI.gif' alt="Spinning fi muni logo..." />
+            </div>
         </div>
     )
 }
