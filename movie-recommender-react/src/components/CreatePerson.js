aa9df41df2fc9c3028330091373ab3dccfade3b2
Kristian Tkacik <xtkacik3@fi.muni.cz>
2021-06-05 19:01:49 +0200

    Change CreatePerson form to use meterial-ui

--- movie-recommender-react/src/components/CreatePerson.js
+++ movie-recommender-react/src/components/CreatePerson.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
-import DatePicker from 'react-date-picker'
 import axios from "axios";
-import {Box, Button, Typography} from "@material-ui/core";
+import {Box, Button, FormLabel, Grid, TextField, Typography} from "@material-ui/core";
 
 /**
  * @author Kristian Tkacik
@@ -10,6 +9,15 @@ export function CreatePerson() {
     const [personName, setPersonName] = useState('');
     const [personBio, setPersonBio] = useState('');
     const [dateOfBirth, setDateOfBirth] = useState('');
+    const [isFormInvalid, setIsFormInvalid] = useState(false);
+
+    const validate = () => {
+        if (personName !== "" && personBio !== "") {
+            setIsFormInvalid(false);
+        } else {
+            setIsFormInvalid(true);
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,9 +28,8 @@ export function CreatePerson() {
             dateOfBirth: dateOfBirth})
             .then(
                 response => JSON.stringify(response))
-            .then(
-                () => alert("Person added successfully"))
-            .catch((msg) => alert(msg))
+            .then(() => alert("Person added successfully"))
+            .catch((msg) => console.log(msg))
     }
 
     return (
@@ -30,49 +37,56 @@ export function CreatePerson() {
             <Box mb={2}>
                 <Typography variant="h4">Create Person</Typography>
             </Box>
-            <form onSubmit={handleSubmit}>
-                <table>
-                    <tr>
-                        <td>
-                            <label>
-                                Name:
-                            </label>
-                        </td>
-                        <td>
-                            <input type="text"
+
+            <form onSubmit={handleSubmit} autoComplete="off">
+                <Grid container spacing={2}>
+
+                    <Grid item xs={2}>
+                        <FormLabel>Name: </FormLabel>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <TextField type="text"
                                    value={personName}
                                    onChange={e => setPersonName(e.target.value)}
+                                   fullWidth
                                    name="name"
                                    placeholder="Enter person name"
-                                   style={{width: 700}} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Bio:
-                            </label>
-                        </td>
-                        <td>
-                            <textarea value={personBio}
-                                      onChange={e => setPersonBio(e.target.value)}
-                                      name="bio"
-                                      placeholder="Enter person bio"
-                                      style={{width: 700, height: 100}} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label>
-                                Date of birth:
-                            </label>
-                        </td>
-                        <td>
-                            <DatePicker value={dateOfBirth} onChange={setDateOfBirth} />
-                        </td>
-                    </tr>
-                    <Button variant="contained" color="primary" type="submit">Submit</Button>
-                </table>
+                                   error={isFormInvalid && personName === ""}
+                                   helperText={isFormInvalid && personName === "" ? "Empty field!" : " "} />
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <FormLabel>Bio: </FormLabel>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <TextField value={personBio}
+                                   onChange={e => setPersonBio(e.target.value)}
+                                   name="bio"
+                                   multiline
+                                   rows={4}
+                                   placeholder="Enter person bio"
+                                   fullWidth
+                                   error={isFormInvalid && personBio === ""}
+                                   helperText={isFormInvalid && personBio === "" ? "Empty field!" : " "} />
+                    </Grid>
+
+                    <Grid item xs={2}>
+                        <FormLabel>Date of birth: </FormLabel>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <TextField type="date"
+                                   value={dateOfBirth}
+                                   onChange={e => setDateOfBirth(e.target.value)}
+                                   fullWidth />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary" type="submit" onClick={validate}>
+                            Submit
+                        </Button>
+                    </Grid>
+
+                </Grid>
             </form>
         </div>
     );
