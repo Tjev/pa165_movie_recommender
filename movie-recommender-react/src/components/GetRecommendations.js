aaf5d46137bfae70d515933871112c9429e88942
Jiří Papoušek <jiri.pap@gmail.com>
2021-06-09 13:43:49 +0200

    Fix showing images in movie recommendations

--- movie-recommender-react/src/components/GetRecommendations.js
+++ movie-recommender-react/src/components/GetRecommendations.js
@@ -5,6 +5,7 @@ import {AddActorLink} from "./AddActorLink";
 import {AddDirectorLink} from "./AddDirectorLink";
 import {Box, Button, Card, CardContent, Grid, Typography} from "@material-ui/core";
 import {formatYear} from "../utils/Common";
+import BrokenImageIcon from '@material-ui/icons/BrokenImage';
 
 /**
  * @author Kristian Tkacik, Jiri Papousek
@@ -16,6 +17,23 @@ function MovieList({ movies, scores, token, setMovies, setScores, setTitle}) {
         return <p>No results found</p>;
     }
 
+    const noImage = () => {
+        return (
+            <Grid container direction="column" style={{
+                "display": "flex",
+                "width": "95%",
+                "height": "95%",
+                "margin": "5px",
+                "alignItems": "center",
+                "justifyContent": "center",
+                "backgroundColor": "white"
+            }}>
+                <BrokenImageIcon style={{fontSize: 150}}/>
+                <Typography>No graphics.</Typography>
+            </Grid>
+        )
+    }
+
     const handleNewRecommend = async (id, title) => {
         setTitle(title);
         await axios.get(`http://localhost:8080/pa165/rest/movies/${id}/recommendations?amount=10`)
@@ -24,58 +42,74 @@ function MovieList({ movies, scores, token, setMovies, setScores, setTitle}) {
                 return axios.all(res.data.map(
                     movie => axios.get(`http://localhost:8080/pa165/rest/movies/${movie.id}/overall-score`).then(res => res.data)));
             })
-            .then( res => {
+            .then(res => {
                 setScores(res);
             })
     }
 
     return (
         <Box mt={1}>
-            <Grid container  spacing={1}>
-                {movies.map(({ id, title, bio, releaseYear, genres, directors, actors }, index) => (
+            <Grid container spacing={1}>
+                {movies.map(({id, title, bio, releaseYear, genres, directors, actors, graphics}, index) => (
                     <Grid item xs={12}>
                         <Card fullwidth style={{backgroundColor: "#e6e6e6"}}>
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" align="left" component="h2">
-                                    {title}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary" align="left" component="p">
-                                    {<p><b>Score: </b> {scores[index]} / 5</p>}
-                                    {<p><b>Bio: </b> {bio}</p>}
-                                    {<p><b>Release year: </b>{ formatYear(releaseYear) }</p>}
-                                    {<p><b>Genres: </b>{genres.map(genre => genre.toString().slice(0, 1) + genre.toString().slice(1, genre.length).toLowerCase()).join(', ')}</p>}
-                                    {<p><b>Directed by: </b>{directors.map(director => director.name).join(', ')}</p>}
-                                    {<p><b>Actors: </b>{actors.map(actor => actor.name).join(', ')}</p>}
-                                    <Grid
-                                        container
-                                        direction="row"
-                                        spacing={2}
-                                    >
-                                        {AddActorLink(id, title, token)}
-                                        {AddDirectorLink(id, title, token)}
+                            <CardContent style={{"paddingBottom": "16px"}}>
+                                <Grid container>
+                                    <Grid item container direction="column" xs={9} justify="space-between">
                                         <Grid item>
-                                            <Button variant="contained" onClick={handleNewRecommend.bind(this, id, title)}>
-                                                <NavLink exact
-                                                         activeClassName="active"
-                                                         to={{
-                                                             pathname:'/get-recommendations',
-                                                             state: {id: id, title: title}
-                                                         }}
-                                                         style={{ textDecoration: 'none', color: 'black' }}>
-                                                    Search for movies like this
-                                                </NavLink>
-                                            </Button>
+                                            <Typography gutterBottom variant="h5" align="left" component="h2">
+                                                {title}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="body2" color="textSecondary" align="left"
+                                                        component="p">
+                                                {<p><b>Score: </b> {scores[index]} / 5</p>}
+                                                {<p><b>Description: </b> {bio}</p>}
+                                                {<p><b>Release year: </b>{formatYear(releaseYear)}</p>}
+                                                {<p>
+                                                    <b>Genres: </b>{genres.map(genre => genre.toString().slice(0, 1) + genre.toString().slice(1, genre.length).toLowerCase()).join(', ')}
+                                                </p>}
+                                                {<p><b>Directed
+                                                    by: </b>{directors.map(director => director.name).join(', ')}</p>}
+                                                {<p><b>Actors: </b>{actors.map(actor => actor.name).join(', ')}</p>}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item container spacing={2}>
+                                            {AddActorLink(id, title, token)}
+                                            {AddDirectorLink(id, title, token)}
+                                            <Grid item>
+                                                <Button variant="contained"
+                                                        onClick={handleNewRecommend.bind(this, id, title)}>
+                                                    <NavLink exact
+                                                             activeClassName="active"
+                                                             to={{
+                                                                 pathname: '/get-recommendations',
+                                                                 state: {id: id, title: title}
+                                                             }}
+                                                             style={{textDecoration: 'none', color: 'black'}}>
+                                                        Search for movies like this
+                                                    </NavLink>
+                                                </Button>
+                                            </Grid>
                                         </Grid>
                                     </Grid>
-                                </Typography>
+                                    <Grid item xs={3} justify="center">
+                                        {graphics
+                                            ? <img src={"data:image/jpg;base64," + graphics}
+                                                   style={{"maxWidth": "95%", "margin": "5px"}}/>
+                                            : noImage()
+                                        }
+                                    </Grid>
+                                </Grid>
                             </CardContent>
+
                         </Card>
                     </Grid>
                 ))}
             </Grid>
         </Box>
     );
-
 }
 
 export function GetRecommendations() {
